ac2da918acdb74db08470cb62ef263daf9ffb4d6
jonesdebu <jonesde@bu.edu>
2022-04-08 15:35:13 -0500

    Oauth2 Integration

---
+++ auth.py
@@ -0,0 +1,123 @@
+import aiohttp.web
+import aiobungie
+import os
+import ssl
+import urllib.parse
+import asyncio
+import motor.motor_asyncio
+
+'''
+Credit to nxtlo for the example on oauth2 with aiohttp and aiobungie:
+https://github.com/nxtlo/aiobungie/blob/399ebd63448ee68256dea639e8a12ce69462f571/examples/user_oauth2.py#L44
+'''
+# Web router
+router = aiohttp.web.RouteTableDef()
+
+
+def parse_url(url: str) -> str:
+    """Parse the url to get authorization code"""
+    parser = urllib.parse.urlparse(url)
+    return parser.query.removeprefix('code=')
+
+
+def parse_discord(url: str) -> str:  # TODO: Need to correctly parse the url for the discord username
+    """Parse the url for discord username"""
+    parser = urllib.parse.urlparse(url)
+    username = url.rsplit('_', 1)[-1]
+    return username
+
+
+client = aiobungie.RESTPool(os.environ.get("api_key"), client_secret=os.environ.get('client_secret'),
+                            client_id=int(os.environ['client_id']))
+
+# mongo_client = motor.motor_asyncio.AsyncIOMotorClient(os.environ.get("MONGO_CONNECTION_URL"))
+#
+# connection_status = mongo_client.test  # Cluster
+# db = mongo_client['Little-Light']  # database
+# collection = db['Users']  # database table
+# print(db)
+# exit()
+
+# implement user queue pop users from queue/dict when they are authenticated
+# create function to update the queue/dict so that discord names can be sent from bot.py
+
+authentication_queue = []
+
+
+# OR
+
+# create mongodb document with discord name in function then update that document
+
+
+def combinational_logic_get_oauth2_url(user):
+    url = client.acquire().build_oauth2_url()
+    url = url + '_' + user  # Hack n' a half: Discord username is appended to the state for the oauth and redirect urls
+    print(url)
+    return url
+
+
+async def get_oauth2_url():
+    async with client.acquire() as rest:
+        oauth_url = rest.build_oauth2_url()
+        print(oauth_url)
+
+
+@router.get("/redirect")
+async def redirect(request: aiohttp.web.Request) -> aiohttp.web.Response:
+    print(str(request.url))
+    discord_name = parse_discord(str(request.url))
+    print(discord_name)
+    if code := parse_url(str(request.url)):
+        code = str(code)
+        tokens = ""
+        async with client.acquire() as rest:
+            tokens = await rest.fetch_oauth2_tokens(code)
+            print(tokens)
+            text_file = open("placeholder.txt", "wt")
+            n = text_file.write(tokens.access_token)
+            text_file.close()
+            request.app["token"] = tokens.access_token
+
+        async with client.acquire() as rest:
+            my_user = await rest.fetch_current_user_memberships(tokens.access_token)
+            print(my_user.get("bungieNetUser").get("uniqueName"))
+
+            # TODO: Store data in db (needs to be done before user starts making calls from bot.py
+            raise aiohttp.web.HTTPFound(location='/me', reason="Oauth2 Success!")
+
+    else:
+        # Otherwise return 404 and couldn't authenticate.
+        raise aiohttp.web.HTTPNotFound(text="Code not found and couldn't authenticate.")
+
+
+@router.get('/me')
+async def me(request: aiohttp.web.Request) -> aiohttp.web.Response:
+    if access_token := request.app.get("token"):
+
+        async with client.acquire() as rest:
+            my_user = await rest.fetch_current_user_memberships(access_token)
+            # print(my_user.get("bungieNetUser").get("uniqueName"))
+            return aiohttp.web.json_response(my_user)
+    else:
+        raise aiohttp.web.HTTPUnauthorized(text="No access token found, Unauthorized.")
+
+
+def start_server():  # TODO: Need to automatically close the server connection (maybe once the tokens are received?)
+    # asyncio.new_event_loop().run_until_complete(get_oauth2_url())
+    # url = combinational_logic_get_oauth2_url()
+    app = aiohttp.web.Application()
+    app.add_routes(router)
+
+    ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+
+    # TODO: Won't need for production (use https server instead of localhost)
+    ctx.load_cert_chain(certfile="../pkeys/cacert.pem", keyfile="../pkeys/little_light_pkey")
+
+    # TODO: Run on cloud localhost
+    aiohttp.web.run_app(app, host="localhost", port=8000, ssl_context=ctx)
+
+
+if __name__ == '__main__':
+    start_server()
+
+# TODO: As a test write a bot function that will give a user's join code
--- bot.py
+++ bot.py
@@ -1,48 +1,36 @@
 import discord
 from discord.ext import commands
-import requests
-import pydest
-import os
-from dotenv import load_dotenv
 import traceback
-#import json
+import json
+import aiobungie
+import auth
+import os
+import asyncio
 
+# OAuth Information
 
-#OAuth Information
-load_dotenv()
-
-apikey = os.getenv("apikey")
-OAuth_client_id = os.getenv("Oauth_Client_Id")
-bungoAuthURI = "https://www.bungie.net/en/OAuth/Authorize?client_id=" + OAuth_client_id + "&response_type=code"
-apiheaders = {'X-API-Key': apikey}
-destiny = pydest.Pydest(apikey)
-
-
-#Discord Bot Client
+# Discord Bot Client
 client = commands.Bot(command_prefix=">", case_insensitive=True)
+
 
 @client.event
 async def on_ready():
-    print("Little Light is ready")
-    print(bungoAuthURI)
-    test_channel = client.get_channel(781576402931417089)
-    await test_channel.send("Little Light is here")
+    print("Lil Light in da house")
+    test_channel = client.get_channel(795189216304037889)
+    await test_channel.send("Lil Light in da house")
+
 
 @client.event
 async def on_disconnect():
     print("Little Light is off")
-    await destiny.close()
-    test_channel = client.get_channel(781576402931417089)
+    test_channel = client.get_channel(795189216304037889)
     await test_channel.send("Little Light disconnected")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if "atraks" in message.content.lower():
-        response = "**BEEP BOOP* \* fuck atraks"
-        await message.channel.send(response, file = discord.File('images/Atraks.jpg'))
 
     elif "raid" in message.content.lower():
         response = "RAID"
@@ -50,90 +38,86 @@ async def on_message(message):
 
     await client.process_commands(message)
 
-#commands
+
+# commands
 @client.command()
-async def pog(ctx): #function name is the command to respond to
+async def pog(ctx):  # function name is the command to respond to
     # This is the bot's response
     try:
-        await ctx.send("\**BEEP BOOP* \* pog indeed", file=discord.File('images/flop.jpg'))
+        await ctx.send("pog indeed", file=discord.File('images/flop.jpg'))
 
-    except:
-        await ctx.send("something went wrong I might not have upload file permissions")
-
-
-
-@client.command()
-async def indeed(ctx): #function name is the command to respond to
-    # This is the bot's response
-    try:
-        await ctx.send("\**BEEP BOOP* \* indeed", file=discord.File('images/Indeed.gif')) # *""* for italics \* escape slash to just use the icon
-
-    except:
-        await ctx.send("something went wrong I might not have upload file permissions")
-
-@client.command()
-async def get(ctx, username=None):
-    if not username:
-        await ctx.send('\**BEEP BOOP* \* Please enter a valid username after the command')
-        return
-    try:
-        id_json = await destiny.api.search_destiny_player(3, username)
-        bungie_id = id_json['Response'][0]['membershipId']
-        #print(bungie_id)
-        myComponents = [200]
-        accounts_json = await destiny.api.get_membership_data_by_id(bungie_id, membership_type=-1)
-        #print(accounts_json)
-        accounts = accounts_json['Response']['destinyMemberships'] # there is a key called primary membership id but i dont know what it does yet so im going to get the specific pc memebrshipId
-        print(accounts)
-        current_account = accounts[0]['membershipId']
-        current_platform = accounts[0]['membershipType']
-        profile_json = await destiny.api.get_profile(current_platform, current_account, myComponents) #platform replaced by a variable
-        #print(profile_json)
-        charData = profile_json['Response']['characters']['data']
-        playerChars = list(charData.keys())
-        print(playerChars)
-        charLevels = []
-        for i in range(len(playerChars)):
-            charLevels.append(str(charData[playerChars[i]]['light']))
-
-        #charLevels.append(str(charData[playerChars[0]]['light'])) #This needs to be in a loop for every char in playerChars
-        """charLevels.append(str(charData[playerChars[1]]['light']))
-        charLevels.append(str(charData[playerChars[2]]['light']))"""
-        level_string = ' '.join(charLevels)
-        #print(testFirstCharLight)
-        await ctx.send('\**BEEP BOOP* \* Your  character light levels are: ' + level_string) # return the first character's light level"""
-
-
-    except (IndexError, KeyError) as e:
+    except Exception as e:
         print(e)
-        await ctx.send('\**BEEP BOOP* \* User not found.')
-        raise
+        await ctx.send("something went wrong I might not have upload file permissions")
+
+
+@client.command()
+async def join(ctx):
+    try:
+        async with auth.client.acquire() as rest:
+            text_file = open("placeholder.txt", "r")
+            token = text_file.read()
+            print(token)
+            text_file.close()
+            my_user = await rest.fetch_current_user_memberships(token)
+            await ctx.send(my_user.get("bungieNetUser").get("uniqueName"))
+
+    except Exception as e:
+        print(e)
+        pass
+
+
+@client.command()
+async def generate_oauth_url(ctx):
+    try:
+        url = auth.combinational_logic_get_oauth2_url(ctx.author.name)
+        await ctx.author.send(url)
+
+    except Exception as e:
+        print(e)
+        await ctx.send("Error check logs")
+
+
+@client.command()
+async def indeed(ctx):  # function name is the command to respond to
+    # This is the bot's response
+    try:
+        await ctx.send("indeed",
+                       file=discord.File('images/Indeed.gif'))  # *""* for italics \* escape slash to just use the icon
+
+    except:
+        await ctx.send("something went wrong I might not have upload file permissions")
 
 
 @client.command()
 async def version(ctx):
     try:
-        myEmbed = discord.Embed(title="Current Version", description="Version is 1.0", color=0x00ff00) #color is hex for RGB RRGGBB so completely green == 00ff00
+        myEmbed = discord.Embed(title="Current Version", description="Version is 1.0",
+                                color=0x00ff00)  # color is hex for RGB RRGGBB so completely green == 00ff00
         myEmbed.add_field(name="Version Code:", value="v1.0.0", inline=False)
         myEmbed.add_field(name="Date Released:", value="11/26/2020", inline=False)
         myEmbed.set_footer(text="Sample Footer")
-        myEmbed.set_author(name="Neo", url="https://github.com/jonesdebu?tab=repositories", icon_url="https://github.githubassets.com/images/modules/open_graph/github-mark.png ")
+        myEmbed.set_author(name="Jonesdebu", url="https://github.com/jonesdebu?tab=repositories",
+                           icon_url="https://github.githubassets.com/images/modules/open_graph/github-mark.png ")
         await ctx.send(embed=myEmbed)
 
     except:
         await ctx.send("Something went wrong I might not have embed permissions")
 
+
 @client.command()
 async def info(ctx):
     try:
-        myEmbed = discord.Embed(title="Help", description="Commands", color=0x000055) #color is hex for RGB RRGGBB so completely green == 00ff00
+        myEmbed = discord.Embed(title="Help", description="Commands",
+                                color=0x000055)  # color is hex for RGB RRGGBB so completely green == 00ff00
         myEmbed.add_field(name="Get Bot Version:", value=">Version", inline=False)
         myEmbed.add_field(name="Pog On Demand:", value=">Pog", inline=False)
         myEmbed.add_field(name="Indeed On Demand:", value=">Indeed", inline=False)
         myEmbed.set_footer(text="Little Light")
         file = discord.File("images/D2Icon.png")
         myEmbed.set_image(url="attachment://D2Icon.png")
-        myEmbed.set_author(name="Neo", url="https://github.com/jonesdebu?tab=repositories", icon_url="https://github.githubassets.com/images/modules/open_graph/github-mark.png ")
+        myEmbed.set_author(name="Jonesdebu", url="https://github.com/jonesdebu?tab=repositories",
+                           icon_url="https://github.githubassets.com/images/modules/open_graph/github-mark.png ")
         await ctx.send(file=file, embed=myEmbed)
 
     except Exception:
@@ -141,8 +125,4 @@ async def info(ctx):
         await ctx.send("Something went wrong I might not have embed permissions")
 
 
-
-
-
-
-client.run(os.getenv("Discord-Bot-Token"))
+client.run(os.environ.get("Discord_Bot_Token"))
